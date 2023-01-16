62369a171fe100a1aa55382648339605523d4e1f
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-01-16 12:19:59 +0800

    Add files via upload
    将一个数组中的奇数放到一个集合中，再将偶数放到另一个集合中，最终将两个集合合并为一个集合，并且奇数显示在左，偶数显示在右。

---
+++ 1_16_ArrayToSet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_16_ArrayToSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int>listou = new List<int>();
+            List<int> listji = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    listou.Add(nums[i]);
+                }
+                else
+                {
+                    listji.Add(nums[i]);
+                }
+
+            }
+            //一种合并方法
+            listji.AddRange(listou);
+            foreach (int item in listji)
+            {
+                Console.Write(item + " ");
+            }
+            //另一种合并方法
+            //List<int>listsum = new List<int>();
+            //listsum.AddRange(listji);
+            //listsum.AddRange(listou);
+            //foreach (int item in listsum)
+            //    {
+            //        Console.Write(item+" " );
+            //    }  
+        } 
+    }
+}
