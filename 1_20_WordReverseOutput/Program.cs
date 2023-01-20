01115dbd3c57fb08bf595bfbeac8462bbba14e57
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-01-20 23:09:00 +0800

    Add files via upload
    单词反向输出

---
+++ 1_20_WordReverseOutput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_20_WordReverseOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] StrNew = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < StrNew.Length / 2; i++)
+            {
+                string temp = StrNew[i];
+                StrNew[i] = StrNew[StrNew.Length - 1-i];
+                StrNew[StrNew.Length - 1 - i] = temp;
+            }
+            str = string.Join("  ", StrNew);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
