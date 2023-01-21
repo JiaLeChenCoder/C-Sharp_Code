d7fb5dd8f9e01f0d4748876d7fa7fc5461967727
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-01-21 22:53:46 +0800

    Add files via upload
    从Email中提取用户名和域名。

---
+++ 1_21_Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_21_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //从email中提取处域名和用户名。
+            string email = "3257853364@qq.com";
+            int index = email.IndexOf('@');
+            string username = email.Substring(0, index);
+            string yuming = email.Substring(index + 1);
+            Console.WriteLine(yuming);
+            Console.WriteLine(username);
+            Console.ReadKey();
+        }
+    }
+}
