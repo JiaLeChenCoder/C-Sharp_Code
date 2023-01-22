bad3ff87112192d3a8b09d4e6f0309f3f40d6562
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-01-22 22:53:29 +0800

    Add files via upload
    Indexof用法，例：输入一句话，找出e的位置。

---
+++ 1_22_Indexof/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_22_Indexof
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //输入一句话，找出e所在的位置
+            string str = Console.ReadLine();
+            int index = str.IndexOf('e');
+            Console.WriteLine("第1次出现e的位置是{0}", index);
+            int count = 1;//用来记录e出现的次数
+            while (index != -1)
+            {
+                count++;
+                index = str.IndexOf('e', index + 1);
+                if (index == -1)
+                {
+                    break;
+                }
+                Console.WriteLine("第{0}次出现e的位置是{1}", count, index);
+            }
+            Console.ReadKey();
+        }
+    }
+}
