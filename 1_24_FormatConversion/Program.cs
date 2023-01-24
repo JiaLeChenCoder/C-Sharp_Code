f5c3bc50a545a66b6aee7964d2e0c34b7839578f
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-01-24 23:14:58 +0800

    Add files via upload
    字符串格式转化

---
+++ 1_24_FormatConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_24_FormatConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //把"你""我""他""她"变成你|我|他|她，然后把|切割
+            string[] names = { "你", "我", "他", "她" };
+            string str = string.Join("|", names);
+            string[]  strNew = str.Split(new char[]{ '|' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
