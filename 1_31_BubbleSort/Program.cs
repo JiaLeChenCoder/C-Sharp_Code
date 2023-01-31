9c4d9cccb4dff1f90a9abd451322b0e87b1efd51
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-01-31 23:11:23 +0800

    Add files via upload
    用冒泡排序对数组升序排列。

---
+++ 1_31_BubbleSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_31_BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 9, 8, 7, 0 };
+            Change(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i]);
+            }
+        }
+        public static void Change(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = 0; j < nums.Length - 1 - i; j++)
+                {
+                    if (nums[j] > nums[j+1])
+                    {
+                        int t = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = t;
+
+                    }
+                }
+            }
+        }
+    }
+}
