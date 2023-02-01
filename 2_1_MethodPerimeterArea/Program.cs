1b5a6ad5f47e72df4a6f143bdb9aba1c2f35d4da
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-01 22:18:48 +0800

    Add files via upload
    计算圆的周长和面积

---
+++ 2_1_MethodPerimeterArea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_1_MethodPerimeterArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //计算圆的周长和面积
+            double r = 5;
+            double perimeter;
+            double area;
+            GetPerimeterArea(r, out perimeter, out area);
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.ReadKey();
+
+        }
+        public static void GetPerimeterArea(double r,out double perimeter,out double area)
+        {
+            perimeter = 2 * 3.14 * r;
+            area = 3.14 * r * r;
+        }
+    }
+}
