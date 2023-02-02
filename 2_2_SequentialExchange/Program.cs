fb9d241cb3eece6b965f2d1d4cce92de37e954c1
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-02 20:11:02 +0800

    Add files via upload
    元素调换

---
+++ 2_2_SequentialExchange/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2_SequentialExchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //将{“橘子”，“香蕉”，“苹果”}中的元素调换
+            string[] names = { "橘子", "香蕉", "苹果" };
+            Test(names);
+            for (int i = 0; i <names.Length; i++)
+            {
+                Console.Write(names[i]);
+            }
+            Console.ReadKey();
+        }
+        public static void Test(string[] names)
+        {
+            for (int i = 0; i < names.Length/2; i++)
+            {
+                string temp = names[i];
+                names[i] = names[names.Length - 1 - i];
+                names[names.Length - 1 - i] = temp;
+            }
+        }
+    }
+}
