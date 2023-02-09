fe34611334288a3cdfce0bbdd1404c344bbf9bef
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-09 20:24:59 +0800

    Add files via upload
    提取用户名和域名

---
+++ 2_9_ExtractUserNameAndDomainName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_9_ExtractUserNameAndDomainName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String email = "333222569@qq.com";
+            int index = email.IndexOf('@');
+            string UserName = email.Substring(0, index);
+            string DomainName = email.Substring(index + 1);
+            Console.WriteLine("用户名是{0}，域名是{1}", UserName, DomainName);
+            Console.ReadKey();
+        }
+    }
+}
