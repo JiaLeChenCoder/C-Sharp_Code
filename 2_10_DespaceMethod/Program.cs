036a3df3d48cf8e7623d6e320f2f539174524e66
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-10 20:21:24 +0800

    Add files via upload
    去空格法Trim

---
+++ 2_10_DespaceMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_10_DespaceMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "  hahaha  ha  ";
+            //str = str.Trim();//去所有的空格
+            //str = str.TrimStart();//去前面空格
+            //str = str.TrimEnd();//去后面空格
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
