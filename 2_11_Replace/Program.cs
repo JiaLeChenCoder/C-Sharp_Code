7760999b5b3e1b8cba9f32f4359c55220e855b88
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-11 23:23:57 +0800

    Add files via upload
    替换关键字Replace,Contains

---
+++ 2_11_Replace/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_11_Replace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "国家关键人物张三";
+            if (str.Contains("张三"))
+            {
+                str = str.Replace("张三", "**");
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
