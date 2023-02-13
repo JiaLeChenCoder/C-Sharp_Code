41b20a2a5d7cadb2f3a95e9ec2910ba176504b2c
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-13 21:10:17 +0800

    Add files via upload
    在集合中存放指定范围的随机数，要求不能重复。

---
+++ 2_13_RandomNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_13_RandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //写一个长度为10的集合，要求在里面随机放10个数字（0-9），要求里面的数字不能重复
+            ArrayList List = new ArrayList();
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int rnumber = r.Next(0, 10);
+                if (!List.Contains(rnumber))
+                {
+                    List.Add(rnumber);
+                }
+                else//集合中有这个随机数
+                {
+                    i--;//一旦产生重复的随机数，这次的循环就不算数
+                }
+            }
+            for (int i = 0; i < List.Count; i++)
+            {
+                Console.WriteLine(List[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
