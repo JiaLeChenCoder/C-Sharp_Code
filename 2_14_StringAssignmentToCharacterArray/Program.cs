08461cd342a593abe4f1d78bbd28097712b37fce
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-14 11:27:04 +0800

    Add files via upload
    通过foreach循环将字符串赋值给字符数组。

---
+++ 2_14_StringAssignmentToCharacterArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_14_StringAssignmentToCharacterArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //提示用户输入一个字符串，通过foreach循环将用户输入的字符串赋值给一个字符数组
+            Console.WriteLine("请输入一个字符串");
+            string input = Console.ReadLine();
+            char[] chs = new char[input.Length];
+            int i = 0;
+            foreach (var item in input)
+            {
+                chs[i] = item;
+                i++;
+            }
+            foreach (var item in chs)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
