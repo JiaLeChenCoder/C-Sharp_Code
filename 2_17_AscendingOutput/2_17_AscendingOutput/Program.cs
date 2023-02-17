58acffa1f703ea06f63a3a34d59f298a216cae36
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-17 21:24:27 +0800

    Add files via upload
    输入的数按升序输出

---
+++ 2_17_AscendingOutput/2_17_AscendingOutput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_17_AscendingOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int i = 0;
+            Console.WriteLine("请输入10个整数");
+            for ( i = 0; i <10; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(array);
+            Console.WriteLine("按升序排列结果如下");
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
