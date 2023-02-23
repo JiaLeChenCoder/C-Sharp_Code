1ebd2000b88137b82e2c357734b7077f18c9c90a
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-23 23:16:02 +0800

    Add files via upload
    水仙花数

---
+++ 2_23_NarcissisticNumber/2_23_NarcissisticNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_23_NarcissisticNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //定义两个int型变量  
+            int number, sum = 0;
+            //循环从100到999
+            for (number = 100; number < 1000; number++)
+            {
+                //拆分每个数字
+                int units, tens, hundreds;
+                units = number % 10;
+                tens = (number / 10) % 10;
+                hundreds = (number / 10) / 10;
+                //每个数字的立方和
+                sum = (int)Math.Pow(units, 3) + (int)Math.Pow(tens, 3) + (int)Math.Pow(hundreds, 3);
+                if (sum == number)
+                {
+                    //输出水仙花数
+                    Console.WriteLine(number + " 是一个水仙花数");
+                }
+            }
+        }
+    }
+}
+
