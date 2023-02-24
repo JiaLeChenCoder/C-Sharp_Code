7ac0c855fe80a5665dfbf0fc9f77e960cbef494c
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-24 22:38:58 +0800

    Add files via upload
    方法实现打招呼

---
+++ 2_24_GreetSb/2_24_GreetSb/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_24_GreetSb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+           Console.WriteLine(Hello(s));
+            Console.ReadKey();
+        }
+        public static string Hello(string name)
+        {
+            return "Hello" + name + "!";
+        }
+    }
+}
