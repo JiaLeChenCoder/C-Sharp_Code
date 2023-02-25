c58f5c096df0a56dc720488ebe6bdd33c4c720f6
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-25 22:35:48 +0800

    Add files via upload
    实现回文塔

---
+++ 2_25_PalindromeTower/2_25_PalindromeTower/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_25_PalindromeTower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n * 2; j++)
+                {
+                    if (i >= n - j && i >= j - n)
+                    {
+                        Console.Write("{0}", j > n ? (i + 1) + n - j : (i + 1) + j - n);
+                    }
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
